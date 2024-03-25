7bd0400422bc0a386239925a5666c1313d7712d4
Aum Pauskar <aumpauskar111@gmail.com>
2024-03-25 20:40:04 +0530

    material ui added

--- frontend/src/DriversChampionshipTable.js
+++ frontend/src/DriversChampionshipTable.js
@@ -1,6 +1,25 @@
 import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+  tableContainer: {
+    marginTop: '2rem',
+  },
+});
 
 const App = () => {
+  const classes = useStyles();
   const [drivers, setDrivers] = useState([]);
 
   useEffect(() => {
@@ -28,25 +47,31 @@ const App = () => {
 
   return (
     <div>
-      <h1>Formula 1 Drivers</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Position</th>
-            <th>Driver</th>
-            <th>Points</th>
-          </tr>
-        </thead>
-        <tbody>
-          {drivers.map(({ driver, points, position }, index) => (
-            <tr key={index}>
-              <td>{position}</td>
-              <td>{driver}</td>
-              <td>{points}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <Typography variant="h3" component="h1" align="center" gutterBottom>
+        Formula 1 Drivers
+      </Typography>
+      <TableContainer component={Paper} className={classes.tableContainer}>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Position</TableCell>
+              <TableCell>Driver</TableCell>
+              <TableCell>Points</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {drivers.map(({ driver, points, position }, index) => (
+              <TableRow key={index}>
+                <TableCell component="th" scope="row">
+                  {position}
+                </TableCell>
+                <TableCell>{driver}</TableCell>
+                <TableCell>{points}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 };
